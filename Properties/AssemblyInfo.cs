dd7ca9d06a69e9e73639f2081a8f43ddd9dd4c20
William Roush <william.roush@roushtech.net>
2014-11-17 19:39:27 -0500

    Initial import.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RoushTech.Microsoft.Dynamics.Import")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RoushTech.Microsoft.Dynamics.Import")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d1cb5cce-e125-4ef3-a2b7-432f4fdab73b")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
